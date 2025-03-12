cd25edcf9adfcb8148ac85c94c0fda5d8da69f54
Mathis Perrier <mathis.perrier73@gmail.com>
2025-03-12 10:21:34 +0000

    Blindly update background color

--- routes/index.tsx
+++ routes/index.tsx
@@ -2,7 +2,7 @@ import { ChipsItem } from "../components/ChipsItem.tsx";
 
 export default function Home() {
   return (
-    <div class="px-4 py-8 mx-auto bg-[#86efac] h-dvh">
+    <div class="px-4 py-8 mx-auto bg-[#ffbf44] h-dvh">
       <div class="max-w-screen-md mx-auto flex flex-col items-center justify-center">
         <img
           class="my-6"
