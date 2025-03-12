55e8de94a6435f989e14dc2a80649bd145b4c3f5
Mathis Perrier <mathis.perrier73@gmail.com>
2025-03-12 10:10:59 +0000

    Blindly change logo

--- routes/index.tsx
+++ routes/index.tsx
@@ -6,13 +6,13 @@ export default function Home() {
       <div class="max-w-screen-md mx-auto flex flex-col items-center justify-center">
         <img
           class="my-6"
-          src="/logo.svg"
+          src="/logo.webp"
           width="128"
           height="128"
-          alt="the Fresh logo: a sliced lemon dripping with juice"
+          alt="Logo officiel de ChipsOfficiel"
         />
         <h1 class="text-4xl font-bold">
-          Bienvenu sur la liste de chips officiel !
+          Bienvenue sur la liste de chips officiel !
         </h1>
         <p class="my-4 italic">
           Approuvé par le créateur de ce site, tout autre personne qui dira le
@@ -21,16 +21,13 @@ export default function Home() {
         <ul class="my-4">
           <ChipsItem prefix="" suffix="" />
           <ChipsItem prefix="Contre" suffix="" />
-          <ChipsItem prefix="Super" suffix="" />
+          <ChipsItem prefix="Sur" suffix="" />
           <ChipsItem prefix="Over" suffix="" />
-          <ChipsItem prefix="Super over" suffix="" />
           <ChipsItem prefix="Turbo" suffix="" />
           <ChipsItem prefix="Mega" suffix="" />
-          <ChipsItem prefix="Turbo mega" suffix="" />
           <ChipsItem prefix="Omega" suffix="" />
-          <ChipsItem prefix="Turb'o'mega" suffix="" />
-          <ChipsItem prefix="Ultra" suffix="" />
           <ChipsItem prefix="Hyper" suffix="" />
+          <ChipsItem prefix="Ultra" suffix="" />
           <ChipsItem prefix="Supreme" suffix="" />
           <ChipsItem prefix="Master" suffix="" />
           <ChipsItem prefix="Galaxie" suffix="" />
